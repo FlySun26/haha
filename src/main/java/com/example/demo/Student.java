3d2a0aefb4693819605e80eba28407e811e999a6
sunxiaofei <1290207750@qq.com>
2022-05-10 11:58:31 +0800

    哈哈

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+/**
+ * @ClassName Student
+ * @Description TODO
+ * @Date 2021/12/15 8:11 PM
+ * @Vertion 1.0
+ **/
+public class Student {
+    private String hah;
+    private Integer xixi;
+    private String hahs;
+}
---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName TestController
+ * @Description TODO
+ * @Date 2022/3/26 21:26
+ * @Vertion 1.0
+ **/
+@RestController
+public class TestController {
+
+}
